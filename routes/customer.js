ccff664013cb80faae20465d0cd67a32cd8bccea
celia22 <celialopez22@gmail.com>
2021-05-11 12:30:41 +0200

    add business list to customer main page. added route to customer.js (customer get:id) and add each on customer mainpage view

--- routes/customer.js
+++ routes/customer.js
@@ -2,48 +2,51 @@ const express = require('express');
 
 const router = express.Router();
 const Customer = require('../models/Customer.model');
-
+const Business = require('../models/Business.model');
 const isUserLoggedIn = require('../middleware/login');
 
-
 router.get('/:id', (req, res, next) => {
-  const {id} = req.params;  
-  Customer.findById({"_id": id})
+  const { id } = req.params;
+  Customer.findById({ _id: id })
   .then(customerFromDB => {
-    console.log("BUSINESS FROM DB: ", customerFromDB);
-    res.render('customer/mainPage', {customerFromDB})
+    console.log('BUSINESS FROM DB: ', customerFromDB);
+    // en algun moment haurem de ficar un find  by alguna cosa, per city, o per tipus de menjar etc
+    Business.find()
+    .then(businessesFromDB => {
+      res.render('customer/mainPage', { customerFromDB, businessesFromDB });
+    })    
+  
   });
-})
 
-
+  
+});
 
 // FIND CUSTOMER BY ID AND RENDER UPDATE FORM // NO FUNCIONA EL GET ¿?¿?¿?¿?¿?
 
 router.get('/:id/edit', (req, res, next) => {
   const { id } = req.params;
-    Customer.findById(id)
-      .then(dbCustomer => {
-        console.log(dbCustomer);
-       res.render('customer/edit-form', { dbCustomer });
-      })
-      .catch(error => next(error));
-  });
-  
-  // UPDATE CUSTOMER DATA //
+  Customer.findById(id)
+    .then(dbCustomer => {
+      console.log(dbCustomer);
+      res.render('customer/edit-form', { dbCustomer });
+    })
+    .catch(error => next(error));
+});
 
-  router.post('/:id/edit', (req, res, next) => {
-    const { id } = req.params;
-    const { firstName, lastName, email, password, city, age  } = req.body;
-    Customer.findByIdAndUpdate(id, { firstName, lastName, email, password, city, age  }, { new: true })
-      .then(dbCustomer => {     
-        res.redirect('/customer');
-        console.log('update', dbCustomer);
-      })
-      .catch(error => {
-        next(error);
-      });
-  });  
+// UPDATE CUSTOMER DATA //
 
+router.post('/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+  const { firstName, lastName, email, password, city, age } = req.body;
+  Customer.findByIdAndUpdate(id, { firstName, lastName, email, password, city, age }, { new: true })
+    .then(dbCustomer => {
+      res.redirect('/customer');
+      console.log('update', dbCustomer);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 // DELETE USER ///
 // router.post('/:id', (req, res, next) => {
