b0ec2242e4acb3445e8e37cf1abd00df2be7b0b6
marina <marinaaltesmagret@gmail.com>
2021-05-13 21:33:22 +0200

    Add user role middleware

--- middleware/login.js
+++ middleware/login.js
@@ -5,23 +5,4 @@ const isUserLoggedIn = (req, res, next) => {
   return res.redirect('/login');
 };
 
-const isAdmin = (req, res, next) => {
-  if (req.session.currentUser && req.session.currentUser.role === 'admin') {
-    return next;
-  }
-  // ...
-};
-
-// function isUserBusiness() => true o false
-
-// const isBusinesLoggedIn = (req, res, next) => {
-//   if (req.session.currentUser) {
-//     if (isUserBusiness(req.session.currentUser)) {
-//       return next();
-//     }
-//     return res.redirect('/user/:id');
-//   }
-//   return res.redirect('/login');
-// }
-
 module.exports = isUserLoggedIn;
--- routes/business.js
+++ routes/business.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const User = require('../models/User.model');
 const Business = require('../models/Business.model');
 const Product = require('../models/Product.model');
-const Order = require('../models/Order.model');
+const isBusiness = require('../middleware/business');
 
 // RENDER ADD BUSINESS FORM
 router.get('/add', (req, res) => res.render('business/add-business'));
+
+// POST NEW BUSINESS TO DATABASE
 router.post('/add', (req, res, next) => {
   const userId = req.session.currentUser._id;
   const { businessName, businessType, image, city } = req.body;
@@ -20,7 +22,7 @@ router.post('/add', (req, res, next) => {
 });
 
 // RENDER BUSINESS HOME PAGE //
-router.get('/profile', (req, res, next) => {
+router.get('/profile', isBusiness, (req, res, next) => {
   Business.findById(req.session.currentUser._id)
     .then(dbBusiness => res.render('business/mainPage', {dbBusiness}))   
     .catch(err => next(err));
@@ -29,7 +31,7 @@ router.get('/profile', (req, res, next) => {
 
 // RENDER BUSINESS EDIT PAGE // 
 
-router.get('/profile/edit', (req, res, next) => {
+router.get('/profile/edit', isBusiness, (req, res, next) => {
     Business.findById(id)
       .then(dbBusiness => res.render('business/edit-form', { dbBusiness }))
       .catch(error => next(error))
@@ -37,23 +39,23 @@ router.get('/profile/edit', (req, res, next) => {
   
   // UPDATE DB BUSINESS DATA //
 
-router.post('/profile/edit', (req, res, next) => {
+router.post('/profile/edit', isBusiness, (req, res, next) => {
   const { businessName, businessType, city, email, password  } = req.body;
   Business.findByIdAndUpdate(id, { businessName, businessType, city, email, password  }, { new: true })
     .then(() => res.redirect('/business'))
     .catch(error => next(error));
 });  
 
-// RENDER ADD PRODUCT FORM //
-router.get('/:id/product', (req, res) => {
-  res.render('business/add-product', {id});
-});
+// // RENDER ADD PRODUCT FORM //
+// router.get('/profile/product', isBusiness, (req, res) => {
+//   res.render('business/add-product', {id});
+// });
 
-router.post('/:id/product', (req, res, next) => {
-  const {price, description} = req.body;
-  Product.create({businessName: id, price, description})
-    .then(()=> res.redirect('/business'))
-    .catch(err => next(err));
-});
+// router.post('/profile/product', isBusiness, (req, res, next) => {
+//   const {price, description} = req.body;
+//   Product.create({businessName: id, price, description})
+//     .then(()=> res.redirect('/business'))
+//     .catch(err => next(err));
+// });
 
 module.exports = router;
