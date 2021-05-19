1a736577ad70654c40c922757287d8a9a2aa6e24
celia22 <celialopez22@gmail.com>
2021-05-19 09:08:40 +0200

    changed passport config

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const logger = require('morgan');
 const session = require('express-session');
 
 const bcrypt = require('bcrypt');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const flash = require("connect-flash")
 const appSession = require('./configs/session');
 
@@ -28,52 +26,7 @@ const orderRouter = require('./routes/order');
 
 // require database configuration
 require('./configs/db.config');
-require('./configs/passport.config');
 
-
-// passport config
-passport.serializeUser((user, done) => {
-  console.log('OK')
-  done(null, user);
-});
-
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-
-// passport.deserializeUser((id, done) => {
-//   User.findById(id)
-//     .then(user => done(null, user))
-//     .catch(err => done(err));
-// });
- 
-
-passport.use
-(new LocalStrategy(
-  { passReqToCallback: true },
-  {
-    usernameField: 'email',
-    passwordField: 'password',
-  },
-
-  (  email, password, done) => {
-    User.findOne({  email })
-      .then(user => {
-        if (!user) {
-         return done (null, false);
-        } 
-        if (!bcrypt.compareSync(password, user.passwordHash)) {
-          return done (null, false )
-        } 
- 
-        return done(null, user)
-        
-      })
-      .catch(err => done(err));
-  },
-),
-);
 
 // Middleware setup
 app.use(logger('dev'));
@@ -83,13 +36,10 @@ app.use(cookieParser());
 app.use(session(appSession));
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Express view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 
 
 app.use('/', indexRouter);
