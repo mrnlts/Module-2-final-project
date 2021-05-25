196d22b716def6f2a745fe954caeacd232fbabf2
marina <marinaaltesmagret@gmail.com>
2021-05-25 12:35:10 +0200

    Add multiple items to order, see order details, add third status to orders, divide order history into open orders -shopping cart- and history

--- routes/order.js
+++ routes/order.js
@@ -8,40 +8,60 @@ const router = express.Router();
 
 router.get('/', isUserLoggedIn, async (req, res, next) => {
   try {
-    const dbOrders = await Order.find({ user: req.session.currentUser._id }).populate('business').populate({ path: 'product', populate: [{ path: 'businessName' }] });
-    // const money = dbOrders.reduce((acc, curr) => acc + curr);
-      res.render('user/order-history', { dbOrders, successMessage: req.flash('success'), orderHistory: true });
+    const dbOpenOrders = await Order.find({ user: req.session.currentUser._id, status: 'open' }).populate('business').populate({ path: 'product', populate: [{ path: 'businessName' }] });
+    const dbClosedOrders = await Order.find({ user: req.session.currentUser._id, status: {$ne: 'open'} }).populate('business').populate({ path: 'product', populate: [{ path: 'businessName' }] });
+    res.render('user/order-history', { dbOrders: {dbOpenOrders, dbClosedOrders}, successMessage: req.flash('success'), orderHistory: true});
   } catch (e) {
     res.render('error404');
     next(e);
   }
 });
 
+router.get('/:id/details', isUserLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const dbOrder = await Order.findById(id).populate('business').populate({ path: 'products', populate: [{ path: 'item' }]});
+    let openOrder = true;
+    if (dbOrder.status !== 'open') {
+      openOrder = false;
+    }
+    res.render('user/order-detail', {dbOrder, successMessage: req.flash('closed'), openOrder});
+  } catch (e) {
+    res.render('error404');
+    next(e);
+  }
+});
+
+router.post('/:id/confirm', isUserLoggedIn, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const dbOrder = await Order.findByIdAndUpdate(id, {status: 'pending'});
+    req.flash('closed', 'Your order has been sent to the restaurant!');
+    res.redirect(`/orders/${id}/details`);
+  } catch (e) {
+    res.render('error404');
+    next(e);
+  }
+});
+
+
 router.post('/', async (req, res, next) => {
   const { product, business } = req.body;
   try {
     let dbOrder = await Order.findOne({business});
     if (!dbOrder) {
-      console.log("THERE ARE NO ORDERS IN THE SELECTED BUSINESS");
-      console.log("current body request: ", req.body);
-      console.log("Current product: ", product);
         dbOrder = Order.create({
         "business": business,
         "products": [{"item": product, amount: 1}],
         "user": req.session.currentUser._id,
-        "status": 'pending',
+        "status": 'open',
       });
     } else {
-      await console.log("ORDER DOES EXIST AND IT IS: ", dbOrder);
       let products = await [...dbOrder.products];
-      await console.log("HERE'S A COPY OF THE CURRENT PRODUCTS IN THE ORDER: ", products, 
-      "AND HERE IS THE PRODUCT THAT WE'RE ABOUT TO PUT IN IT", product);
       await products.push({"amount": 1, "item": product});
-      await console.log("THIS IS WHAT THE LISH LOOKS LIKE AFTER WE PUSH THE CURRENT PRODUCT: ", products);
-      const newOrder = await Order.findOneAndUpdate(business, {"products": products});
-      console.log("THIS IS THE UPDATED ORDER: ", newOrder);
+      await Order.findOneAndUpdate(business, {"products": products});
     }
-    req.flash('success', 'Your order is confirmed');
+    req.flash('success', 'Product added to shopping cart');
     res.redirect('/orders');
   } catch (e) {
     res.render('error500');
