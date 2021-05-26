8da96e7222681461c8bc158c19eefffc96c2147a
celia22 <celialopez22@gmail.com>
2021-05-26 09:32:53 +0200

    commit to pull

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
-const express = require('express');
-
 const passport = require('passport');
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const express = require('express');
 
 const router = express.Router();
 const bcryptjs = require('bcryptjs');
